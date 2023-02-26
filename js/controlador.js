068fbfa75ea06002bee1b246a617000408613cb6
DeboBastian <cursosdeprogramacion00@gmail.com>
2023-02-26 20:38:06 +0100

    Funciones de busqueda semantica y añadir. No funcionan muy bien pero no las quiero romper

--- js/controlador.js
+++ js/controlador.js
@@ -1,31 +1,46 @@
 
+//BORRAR TAREAS
 
-// function addTarea(Listatareas, pNewTarea) {
-    
-//     let existe = pEmployeeList.some(employee => employee.email === pEmployee.email)
-
-//     if (!existe) {
-//         pEmployee.id = ID;
-//         pEmployeeList.push(pEmployee);
-//         ID++;
-//         console.log(pEmployeeList);
-//         return { status: true, msg: "" }
-//     } else {
-//         return { status: false, msg: "Usuario duplicado" }
-//     }
-
-// }
-
-
-
- function deleteTarea(pListTareas, pId) {
+function deleteTarea(pListTareas, pId) {
 
     let posicion = pListTareas.findIndex(tarea => tarea.id === pId);
-   if (posicion !== -1) {
+    if (posicion !== -1) {
         pListTareas.splice(posicion, 1);
-         return { status: true, msg: 'Tarea borrada correctamente' }
+        return { status: true, msg: 'Tarea borrada correctamente' }
     }
-     return { status: false, msg: 'No se ha podido borrar la tarea' }
- }
+    return { status: false, msg: 'No se ha podido borrar la tarea' }
+}
 
 // deleteTarea(Listatareas, parseInt(1))
+
+
+
+//BUSCAR TAREAS
+
+function busquedaTarea(pListTareas, pLetra) {
+    return pListTareas.filter(tarea => tarea.nombre.toLowerCase().includes(pLetra.toLowerCase()))
+   
+}
+
+//console.log(busquedaTarea(Listatareas, 'e'))
+
+
+
+
+//AÑADIR TAREAS
+
+function NewTarea(pListTareas, pnombreTarea, pPrioridad) {
+    let newTarea = {
+       id: idGlobal,
+        nombre: pnombreTarea,
+        prioridad: pPrioridad
+    }
+
+     pListTareas.push(newTarea);
+ idGlobal++;
+     return newTarea
+ }
+
+
+NewTarea(Listatareas, 'poner lavadora', 'urgente')
+
--- js/vista.js
+++ js/vista.js
@@ -1,17 +1,26 @@
 
 
 const sectionTareas = document.querySelector('#tareas');
-const borrar = document.querySelectorAll('button');
+const guardar = document.querySelector('#guardar');
+const inputSearch = document.querySelector('#formSearch');
+const addTarea = document.querySelector('#newToDo');
+
 
 
 
 function printTareas(pListTareas, pSectionDom) {
 
-  pSectionDom.innerHTML = '';
-     pListTareas.forEach(tarea => pintOneTarea(tarea, pSectionDom))
+    pSectionDom.innerHTML = '';
+    if (pListTareas.length > 0) {
 
+        pListTareas.forEach(tarea => pintOneTarea(tarea, pSectionDom))
+        return true;
+    }
+    pSectionDom.innerHTML = "<h2>No hay tareas que mostrar</h2>";
+    return false;
 }
 
+// REVISAR NO HAY TAREAS QUE MOSTRAR - NO FUNCIONA
 
 function pintOneTarea(pTarea, pDom) {
 
@@ -24,30 +33,73 @@ function pintOneTarea(pTarea, pDom) {
     h3.innerHTML = `${pTarea.nombre}`;
     button.innerHTML = 'BORRAR';
     hr.innerHTML = '<hr>';
+    // HR NO FUNCIONA
 
     button.addEventListener('click', removeTarea);
-    // button.dataset.id = pTarea.id;
 
     article.appendChild(h3);
     article.appendChild(button);
     article.appendChild(hr);
     pDom.appendChild(article);
-    
+
 }
 
 printTareas(Listatareas, sectionTareas)
 
-
+// REVISAR EVENT . MARCA UNDEFINED
 
 function removeTarea(event) {
-  let idBorrar = parseInt(event.target.dataset.id);
-let result = deleteTarea(Listatareas, idBorrar);
-  if (result){
-    event.target.parentNode.remove();
-    alert('Tarea borrada correctamente')
-}else {
+    let idBorrar = event.target.id;
+    let result = deleteTarea(Listatareas, idBorrar);
+    if (result) {
+        event.target.parentNode.remove();
+        alert('Tarea borrada correctamente')
+    } else {
         alert('Fallo al borrar la tarea')
-  }
+    }
 }
 
-removeTarea()
+//removeTarea()
+
+
+
+
+inputSearch.addEventListener('input', busqueda)
+
+
+function busqueda(event) {
+    event.preventDefault();
+
+    let letra = event.target.value;
+    let listaFiltrada = busquedaTarea(Listatareas, letra);
+    if (listaFiltrada.length === 0) {
+        alert('No se encontraron resultados')
+    } else {
+        printTareas(listaFiltrada, sectionTareas)
+    }
+
+}
+
+
+
+
+addTarea.addEventListener('submit', getDataForm)
+
+function getDataForm(event) {
+    event.preventDefault();
+
+
+    let nombre = event.target.new.value;
+    let prioridad = event.target.priority.value
+
+    let result = NewTarea(Listatareas, nombre, prioridad);
+    if (result) {
+        alert('Tarea agregada correctamente')
+    } else {
+        alert('Fallo al agregar la tarea')
+    }
+
+
+}
+
+//getDataForm()
